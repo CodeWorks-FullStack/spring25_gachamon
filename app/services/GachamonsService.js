31387b1b42960abf28211048f7bb769bfb8b58ac
Jeremy Fowler <jembyfowler@gmail.com>
2025-02-25 11:34:11 -0700

    got the gachamon that we want to set acive

--- app/services/GachamonsService.js
+++ app/services/GachamonsService.js
@@ -1,6 +1,13 @@
+import { AppState } from "../AppState.js";
+
 class GachamonsService {
   setActiveGachamon(name) {
     console.log(`Setting ${name} as the active gachamon`);
+    const gachamons = AppState.gachamons
+
+    const foundGachamon = gachamons.find(gachamon => gachamon.name == name)
+    console.log('Here they are!', foundGachamon);
+
   }
 }
 
