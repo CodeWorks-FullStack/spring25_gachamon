e10cdfce8a5d8e89a085f89e0a982c544005c8d8
Jeremy Fowler <jembyfowler@gmail.com>
2025-02-25 12:16:56 -0700

    added coinscontroller to app

--- app/App.js
+++ app/App.js
@@ -1,7 +1,9 @@
+import { CoinsController } from "./controllers/CoinsController.js"
 import { GachamonsController } from "./controllers/GachamonsController.js"
 
 class App {
   gachamonsController = new GachamonsController()
+  coinsController = new CoinsController()
 }
 
 window['app'] = new App()
