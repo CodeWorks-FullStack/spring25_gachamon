e4a73449c3aa8ad0066f9925e32cd8de8a6dfac6
Jeremy Fowler <jembyfowler@gmail.com>
2025-02-25 09:35:06 -0700

    made gachamon model

---
+++ app/models/Gachamon.js
@@ -0,0 +1,17 @@
+
+export class Gachamon {
+  constructor(gachamonName, gachamonEmoji, gachamonType, gachamonRarity) {
+    this.name = gachamonName
+    this.emoji = gachamonEmoji
+    this.type = gachamonType
+    this.rarity = gachamonRarity
+  }
+
+  name = ''
+  emoji = ''
+  type = ''
+  rarity = 0
+  amountInInventory = 0
+}
+
+// example of how to use class: new Gachamon('Barry ', '🦈', 'biter', 2)
